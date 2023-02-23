61121b0e7f1935be7526c8be66dbdf837c79583f
Arian Jalalian <arian.jln.arian@gmail.com>
2023-02-23 14:18:29 +0330

    print function implemented

--- Lab1/Codes/A1.asm
+++ Lab1/Codes/A1.asm
@@ -1,7 +1,52 @@
 
 .data 
     num dw 0
+    ten dw 10  
+    result dw 1489   
+    counter dw 0  
+    
 
+print proc near:
+             
+          push ax ; store ax
+          push dx ; store dx
+            
+          mov ax, result ; mov result to ax to be numerator
+                  
+          start_print: 
+            
+            mov dx, 0 ; empty dx
+            div ten ; the remainder will be in dx 
+                
+            add dx, 48 ; making the ascii code 
+            push dx ; save the digit 
+            inc counter ; increment the counter
+             
+            cmp ax, 0 ; compare the quotient to 0 
+            jnz start_print ; if it is not 0 repeat 
+            
+          end_print: 
+            
+            pop dx ; pop the digit 
+            
+            mov ax, 200h  
+            int 21h ; print  
+            
+            dec counter ; decrement counter 
+            cmp counter, 0 ; all of the digits are printed 
+            
+            jnz end_print ; repeat if counter != 0 
+            
+          
+          pop dx ; restore dx 
+          pop ax ; restore ax
+          
+                
+          ret
+          
+print endp    
+
+        
 
 input proc near:       
     
@@ -10,13 +55,13 @@ input proc near:
     
     push 0 ; current num
     
-    start: 
+    start_input: 
     
         mov ah, 01h ; setting ah to input
 		int	21h ; intrupt to input, result in al
 		
 		cmp al, 13 ; set zf 1 if al is an enter character  
-        jz end ; if zero flag is 1 goto end 
+        jz end_input ; if zero flag is 1 goto end 
         
         sub al, 48 ; ascii - 48 is the actual integer  
         pop bx ; loading current num to bx 
@@ -35,21 +80,22 @@ input proc near:
         push ax ; saving new current num 
           
     
-        jmp start ; back to start
+        jmp start_input ; back to start
     
-    end: 
+    end_input: 
         
         pop num ; inputed number now is in num     
     
     
     pop bx ; restore bx 
-    pop ax ; restore ax        
+    pop ax ; restore ax  
+    
+    ret        
         
 input endp     
-    
-    
 
-ret
+
+
 
 
 
